0d124326d084dd5c5529a3269548db7f449d87ae
Jarrett Lusso <jclusso@graphicflash.com>
2013-07-12 02:20:14 -0400

    .

--- lib/show_message/view_helpers.rb
+++ lib/show_message/view_helpers.rb
@@ -2,12 +2,12 @@ module ShowMessage
   module ViewHelpers
 
     def show_message(options = {}, *args)
-      
+
       options = {autohide: false}.merge!(options) 
       if options[:id].present?
         class_mappings = {"success_#{options[:id]}".to_sym => "success", "error_#{options[:id]}".to_sym => "error", "warning_#{options[:id]}".to_sym => "warning", "info_#{options[:id]}".to_sym => "info"} 
         flash_is_empty = false
-        class_mappings.each_key do |k|\
+        class_mappings.each_key do |k|
           flash_is_empty = false unless flash[k].nil?
         end
         return if flash_is_empty
