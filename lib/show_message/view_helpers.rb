2e0640eaecec90f178857ba476fdcd46fa13004c
Jarrett Lusso <jclusso@graphicflash.com>
2013-07-12 02:11:48 -0400

    Added show_message options

--- lib/show_message/view_helpers.rb
+++ lib/show_message/view_helpers.rb
@@ -1,6 +1,8 @@
 module ShowMessage
   module ViewHelpers
-    def show_message
+
+    def show_message(options = {}, *args)
+      
       options = {autohide: false}.merge!(options) 
       if options[:id].present?
         class_mappings = {"success_#{options[:id]}".to_sym => "success", "error_#{options[:id]}".to_sym => "error", "warning_#{options[:id]}".to_sym => "warning", "info_#{options[:id]}".to_sym => "info"} 
@@ -33,37 +35,8 @@ module ShowMessage
       end
       class_mappings.each_key { |k| flash[k] = nil }
 
-      render :partial => "helpers/show_message", :locals => {data: data, options: options}
+      render :partial => "show_message/show_message", :locals => {data: data, options: options}
     end
 
-    # def placeholder_image_tag(options = {})
-    #   source = "http://lorempixum.com"
-    #   options[:alt] = "lorempixum" unless options.has_key?(:alt)
-
-    #   # gray
-    #   if options.has_key?(:gray)
-    #     source += "/g" if options[:gray]
-    #     options.delete(:gray)
-    #   end
-
-    #   # width
-    #   width = options.has_key?(:width) ? options[:width] : 100
-    #   source += "/#{width}"
-
-    #   # height
-    #   height = options.has_key?(:height) ? options[:height] : 100
-    #   source += "/#{height}"
-
-    #   # category
-    #   if options.has_key?(:category)
-    #     source += "/#{options.delete(:category)}"
-
-    #     # number
-    #     source += "/#{options.delete(:number)}" if options.has_key?(:number)
-    #   end
-
-    #   image_tag(source, options)
-    # end
-    # alias_method :lorempixum_image_tag, :placeholder_image_tag
   end
 end
